cf6594d2de6b4c0ac4f5240cb606088b4d2d20f5
Cristian Gonzalez <82175293+n0gu1@users.noreply.github.com>
2025-05-27 02:49:34 -0600

    Corrige ubicación de archivos estáticos para Render

--- Ejercicio/settings.py
+++ Ejercicio/settings.py
@@ -15,7 +15,7 @@ ALLOWED_HOSTS = ["*"]
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "Ejercicio", "web", "static"),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 # Aplicaciones Django
